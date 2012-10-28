800831735732c52df704f66bae1f0f4ff7a728f0
Nadav Kavalerchik <nadavkav@gmail.com>
2012-10-28 13:01:55 +0200

    new Hebrew translation

---
+++ lang/he/subcourse.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['errinvalidrefcourse'] = 'Error: Invalid ID of referenced course. The referenced course has been probably deleted.';
+$string['errlocalremotescale'] = 'Unable to fetch grades: the remote final grade item uses local scale.';
+$string['errnonnumeric'] = 'Non-numeric argument';
+$string['hiddencourse'] = '*חבוי*';
+$string['modulenameplural'] = 'ציונים מתת־קורסים';
+$string['modulename'] = 'ציונים מתת־קורס';
+$string['pluginname'] = 'ציונים מתת־קורס';
+$string['pluginadministration'] = 'ניהול ציונים מתת־קורס';
+$string['refcourselabel'] = 'יבוא ציונים מ';
+$string['refcourse'] = 'קישור לקורס';
+$string['subcourseintro'] = 'תאור';
+$string['subcoursename'] = 'כותרת';
+$string['gotocoursename'] = 'קישור לקורס <a href="{$a->href}">{$a->name}</a>';
+$string['lastfetchtime'] = 'עדכון אחרון: {$a}';
+$string['lastfetchnever'] = 'הציונים טרם עודכנו';
+$string['fetchnow'] = 'עדכון ציונים, כעת';
+$string['refcourse_help'] = 'The referenced course is the one the grade of the activity is taken from. Students should be enroled into the referenced course.';
+$string['mods_help'] = 'The module provides very simple yet useful functionality. When added into
+  a course, it behaves as a graded activity. The grade for each student is taken
+  from a final grade in another course. Combined with metacourses, this allows
+  course designers to organize courses into separate units.';
+$string['index_help'] = '';
+$string['currentgrade'] = 'ציון נוכחי:';
+$string['addmeta'] = 'הוספת מנגנון רישום "קורס־אב" לתת־קורס המקושר';
+$string['addqual'] = 'הוספת מנגנון רישום "עמידה בתנאים" לתת־קורס המקושר';
+$string['compulsory'] = 'האם לאלץ את התלמידים לבצע את תת־הקורס המקושר?';
+$string['enrolled'] = 'הנך רשום לקורס זה בעזרת מנגנון הרישום: {$a}';
+$string['optionalenrolled'] = 'הנך רשום לקורס זה, קורס זה אינו קורס חובה ואינך מחוייב להשלים את הפעילויות בקורס זה';
+$string['optionalnotenrolled'] = 'זהו קורס העשרה (אינו חובה), אינך רשום לקרוס זה';
+$string['cannotenrol'] = 'אינך רשום או יכול להירשם לקורס זה';
+$string['gotoassmgr'] = 'קישור למנהל משימות {$a}';
+$string['compulsoryqual'] = 'תת־הקורס זה מאלץ מנגנון רישום {$a->enrol} מובנה, בעל {$a->count} משתמשים רשומים.';
+$string['optionalqual'] = 'לתת־הקורס יש מנגנון רישום {$a->enrol} מובנה, בעל {$a->count} משתמשים רשומים.';
+$string['enrol'] = 'רישמו אותי';
+$string['noenrolment'] = 'ללא הוספת מנגנון רישום לקורס';
+$string['instantredirect'] = 'האם להציג את הקורס המקושר ללא עמוד בייניים?';
+$string['addenrolment'] = 'האם להוסיף מנגנון רישום לקורס?';
+
+
+
+
+
+
+
+
