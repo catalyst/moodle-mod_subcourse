b748f620c7f75f1ed116c45a771228286e1fc7c8
David Mudrák <david@moodle.com>
2014-12-10 14:47:59 +0100

    Fix missing name of the addinstance capability

--- lang/en/subcourse.php
+++ lang/en/subcourse.php
@@ -51,6 +51,7 @@ You need to be a teacher in the course to have it listed here. You may want to a
 $string['refcoursecurrent'] = 'Keep current reference';
 $string['refcourselabel'] = 'Fetch grades from';
 $string['refcoursenull'] = 'No referenced course configured';
+$string['subcourse:addinstance'] = 'Add a new subcourse';
 $string['subcourse:begraded'] = 'Receive grade from the referenced course';
 $string['subcourse:fetchgrades'] = 'Fetch grades manually from the referenced course';
 $string['subcoursename'] = 'Subcourse name';
